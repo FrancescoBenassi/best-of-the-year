fd4144281559cf55f3d49e5b310ce525ac9058b2
FrancescoBenassi <francybenna77@gmail.com>
2025-02-07 21:16:43 +0100

    Added new project with PageController

---
+++ src/main/java/org/java/lessons/spring_mvc/best_of_the_year/controller/PageController.java
@@ -0,0 +1,19 @@
+package org.java.lessons.spring_mvc.best_of_the_year.controller;
+
+import java.util.ArrayList;
+
+import org.java.lessons.spring_mvc.best_of_the_year.classes.Movie;
+import org.java.lessons.spring_mvc.best_of_the_year.classes.Song;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/")
+public class PageController {
+
+    
+
+}
